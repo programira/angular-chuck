b449bcd9e0c48d06372af1f275e4c1dc0e9f1a49
Mirjana Milosevic <mira.programira@gmail.com>
2021-03-03 17:34:17 +0100

    Implemented caching for categories

--- src/app/shared/services/categories.service.ts
+++ src/app/shared/services/categories.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, publishReplay, refCount } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Category, CategoriesPayload } from 'src/app/shared/models/categories'
+import { Category } from 'src/app/shared/models/categories'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
+  cachedCategories: Observable<Category[]> | undefined;
+
   constructor(
     private http: HttpClient
   ) { }
 
   getCategories() : Observable<Category[]> {
-    console.log("Get Categories");
-    return this.http
-      .get<Category[]>(`${environment.apiUrl}/categories`)
-      .pipe(
-        catchError(this.handleError<Category[]>('getCategories', []))
-      );
+
+    // Cache it once if cachedCategories value is not defined
+    if (!this.cachedCategories) {
+      console.log("Get Categories");
+      this.cachedCategories = this.http
+        .get<Category[]>(`${environment.apiUrl}/categories`)
+        .pipe(
+          catchError(this.handleError<Category[]>('getCategories', [])),
+          publishReplay(1), // tells Rx to cache the latest emitted
+          refCount() // tells Rx to keep the Observable alive as long as there are any Subscribers
+        );
+    }
+    return this.cachedCategories;
+  }
+
+  clearCache() {
+    this.cachedCategories = undefined;
   }
 
   /**
